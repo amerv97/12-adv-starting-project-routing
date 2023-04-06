aa8315c20bf8fa3ff60400820c72ece9a985a433
Amer Varajic <varajicamer@gmail.com>
2023-04-06 14:35:58 +0200

    simulating loading while fetching (deteted)

--- frontend/src/pages/root.js
+++ frontend/src/pages/root.js
@@ -2,12 +2,19 @@ import React from "react";
 import MainNavigation from "../components/MainNavigation";
 import { Outlet } from "react-router-dom";
 // defines where shoud content of children pages gonna be rendered
+import { useNavigation } from "react-router-dom";
+// lets us find out whether we're currently in an active transition, or we are loading data or if we have no active transition going on 
+
 
 function RootLayout() {
+  // const navigation = useNavigation();
+  // navigation.state === ''
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
