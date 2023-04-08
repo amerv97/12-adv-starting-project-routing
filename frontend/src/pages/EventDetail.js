8fb34dd16f9a5dcf3f8925ed1cb0542b29bd5cd8
Amer Varajic <varajicamer@gmail.com>
2023-04-08 14:08:44 +0200

    useRouteLoadDta() Accessing Data From Other Routes

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,8 +1,8 @@
-import { json, useParams, useLoaderData } from "react-router-dom";
+import { json, useParams, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData("event-detail");
   // const params = useParams();
   return (
     <>
