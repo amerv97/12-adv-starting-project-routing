7b073a22c71493b3c4db14bcc0f7a9e9245de738
Amer Varajic <varajicamer@gmail.com>
2023-04-08 13:32:26 +0200

    Dynamic Routes & loader()s

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,33 @@
-import { useParams } from "react-router-dom";
+import { json, useParams, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
+  // const params = useParams();
   return (
     <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
+      <EventItem event={data.event} />
+      
+      {/* <h1>EventDetailPage</h1>
+      <p>Event ID: {params.eventId}</p> */}
     </>
   );
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
