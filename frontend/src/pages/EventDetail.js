e18925d403bf8fc16b97dd7f7c647a682655c023
Amer Varajic <varajicamer@gmail.com>
2023-04-08 15:56:32 +0200

    Submitting Data Programmatically

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,9 @@
-import { json, useParams, useRouteLoaderData } from "react-router-dom";
+import {
+  json,
+  redirect,
+  useParams,
+  useRouteLoaderData,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
@@ -7,7 +12,7 @@ function EventDetailPage() {
   return (
     <>
       <EventItem event={data.event} />
-      
+
       {/* <h1>EventDetailPage</h1>
       <p>Event ID: {params.eventId}</p> */}
     </>
@@ -30,4 +35,21 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not delete event" },
+      {
+        status: 500,
+      }
+    );
+  }
+  return redirect("/events");
 }
